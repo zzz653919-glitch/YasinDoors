8984f7095638b5172c27a78156dfa4f21f55ca96
zzz653919-glitch <zzz653919@gmail.com>
2026-09-08 17:11:57 +0500

    Delete telegram-config.js

--- telegram-config.js
+++
@@ -1,8 +0,0 @@
-/**
- * TELEGRAM SOZLAMALARI
- * -----------------------------------------------------------------
- * Bu qiymatlar allaqachon to'ldirilgan va ishlashga tayyor.
- * -----------------------------------------------------------------
- */
-window.TELEGRAM_BOT_TOKEN = '8919097362:AAFvgfRWGg4ZIIJ9Pi0bsxxPVCqSIqI_eLw';
-window.TELEGRAM_CHAT_ID = '170310198';
